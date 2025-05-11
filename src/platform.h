92e2c657ee5a261e6df1bb9a2c183a7ab945f7b6
tayfyvz <tyfn51@gmail.com>
2025-05-11 18:30:20 -0400

    Hot code reloading imported.

--- src/platform.h
+++ src/platform.h
@@ -1,9 +1,8 @@
-#pragma once
+#pragma  once
 
 // #############################################################################
 //                           Platform Globals
 // #############################################################################
-
 static bool running = true;
 
 // #############################################################################
@@ -13,3 +12,6 @@ bool platform_create_window(int width, int height, char* title);
 void platform_update_window();
 void* platform_load_gl_function(char* funName);
 void platform_swap_buffers();
+void* platform_load_dynamic_library(char* dll);
+void* platform_load_dynamic_function(void* dll, char* funName);
+bool platform_free_dynamic_library(void* dll);
