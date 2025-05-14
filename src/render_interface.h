5d9283f3276ae1737723aa384400206ecb95d2d7
tayfyvz <tyfn51@gmail.com>
2025-05-14 17:40:24 -0400

    Auto tiling using Bitmask

--- src/render_interface.h
+++ src/render_interface.h
@@ -7,12 +7,10 @@
 // #############################################################################
 //                           Renderer Constants
 // #############################################################################
-constexpr int MAX_TRANSFORMS = 1000;
 
 // #############################################################################
 //                           Renderer Structs
 // #############################################################################
-
 struct OrthographicCamera2D
 {
   float zoom = 1.0f;
@@ -33,16 +31,13 @@ struct RenderData
   OrthographicCamera2D gameCamera;
   OrthographicCamera2D uiCamera;
 
-  int transformCount;
-  Transform transforms[MAX_TRANSFORMS];
+  Array<Transform, 1000> transforms;
 };
 
 // #############################################################################
 //                           Renderer Globals
 // #############################################################################
 static RenderData* renderData;
-
-
 
 // #############################################################################
 //                           Renderer Untility
@@ -51,28 +46,42 @@ IVec2 screen_to_world(IVec2 screenPos)
 {
   OrthographicCamera2D camera = renderData->gameCamera;
 
-  int xPos = screenPos.x / 
-             input->screenSize.x * 
-             (int)camera.dimensions.x; // [0; dimensions.x]
+  int xPos = (float)screenPos.x / 
+             (float)input->screenSize.x * 
+             camera.dimensions.x; // [0; dimensions.x]
 
   // Offset using dimensions and position
   xPos += -camera.dimensions.x / 2.0f + camera.position.x;
 
-  int yPos = screenPos.y / 
-             input->screenSize.y * 
-             (int)camera.dimensions.y; // [0; dimensions.y]
+  int yPos = (float)screenPos.y / 
+             (float)input->screenSize.y * 
+             camera.dimensions.y; // [0; dimensions.y]
 
   // Offset using dimensions and position
-  yPos += -camera.dimensions.y / 2.0f - camera.position.y;
+  yPos += camera.dimensions.y / 2.0f + camera.position.y;
 
   return {xPos, yPos};
 }
 
-
-
 // #############################################################################
 //                           Renderer Functions
 // #############################################################################
+void draw_quad(Transform transform)
+{
+  renderData->transforms.add(transform);
+}
+
+void draw_quad(Vec2 pos, Vec2 size)
+{
+  Transform transform = {};
+  transform.pos = pos - size / 2.0f;
+  transform.size = size;
+  transform.atlasOffset = {0, 0};
+  transform.spriteSize = {1, 1}; // Indexing into white
+
+  renderData->transforms.add(transform);
+}
+
 void draw_sprite(SpriteID spriteID, Vec2 pos)
 {
   Sprite sprite = get_sprite(spriteID);
@@ -83,7 +92,7 @@ void draw_sprite(SpriteID spriteID, Vec2 pos)
   transform.atlasOffset = sprite.atlasOffset;
   transform.spriteSize = sprite.spriteSize;
 
-  renderData->transforms[renderData->transformCount++] = transform;
+  renderData->transforms.add(transform);
 }
 
 void draw_sprite(SpriteID spriteID, IVec2 pos)
--- src/win32_platform.cpp
+++ src/win32_platform.cpp
@@ -1,46 +1,42 @@
+
 #include "platform.h"
 #include "tadpole_lib.h"
 
 #define WIN32_LEAN_AND_MEAN
-#define NOMINAX
+#define NOMINMAX
 #include <windows.h>
-
-#include <GL/gl.h>
-#include "glext.h"
 #include "wglext.h"
-
 
 // #############################################################################
 //                           Windows Globals
 // #############################################################################
 static HWND window;
 static HDC dc;
-static PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT_ptr;
 
 // #############################################################################
 //                           Platform Implementations
 // #############################################################################
 LRESULT CALLBACK windows_window_callback(HWND window, UINT msg,
-                                        WPARAM wParam, LPARAM lParam)
+                                         WPARAM wParam, LPARAM lParam)
 {
-    LRESULT result = 0;
+  LRESULT result = 0;
 
-    switch (msg)
-    {
+  switch(msg)
+  {
     case WM_CLOSE:
     {
-        running = false;
-        break;
+      running = false;
+      break;
     }
-    
+
     case WM_SIZE:
     {
-        RECT rect = {};
-        GetClientRect(window, &rect);
-        input->screenSize.x = rect.right - rect.left;
-        input->screenSize.y = rect.bottom - rect.top;
+      RECT rect = {};
+      GetClientRect(window, &rect);
+      input->screenSize.x = rect.right - rect.left;
+      input->screenSize.y = rect.bottom - rect.top;
 
-        break;
+      break;
     }
 
     case WM_KEYDOWN:
@@ -85,225 +81,221 @@ LRESULT CALLBACK windows_window_callback(HWND window, UINT msg,
 
     default:
     {
-        // Let window handle the default input for now
-        result = DefWindowProcA(window, msg, wParam, lParam);
+      // Let windows handle the default input for now
+      result = DefWindowProcA(window, msg, wParam, lParam);
     }
-}
+  }
 
-return result;
+  return result;
 }
 
 bool platform_create_window(int width, int height, char* title)
 {
-    HINSTANCE instance = GetModuleHandleA(0);
+  HINSTANCE instance = GetModuleHandleA(0);
 
-    WNDCLASSA wc = {};
+  WNDCLASSA wc = {};
+  wc.hInstance = instance;
+  wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);       // This means we decide the look of the cursor(arrow)
+  wc.lpszClassName = title;                       // This is NOT the title, just a unique identifier(ID)
+  wc.lpfnWndProc = windows_window_callback;       // Callback for Input into the Window
 
-    wc.hInstance = instance;
-    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.lpszClassName = title; // this is not the title, just a unique identifier(ID)
-    wc.lpfnWndProc = windows_window_callback; // Callback for input the Window
+  if(!RegisterClassA(&wc))
+  {
+    return false;
+  }
 
-    if (!RegisterClassA(&wc))
+  // WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX
+  int dwStyle = WS_OVERLAPPEDWINDOW;
+
+  PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
+  PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr;
+
+  // Fake Window initializing OpenGL
+  {
+    window = CreateWindowExA(0, title, // This references lpszClassName from wc
+                            title,    // This is the actual Title
+                            dwStyle,
+                            100,
+                            100,
+                            width,
+                            height,
+                            NULL,     // parent
+                            NULL,     // menu
+                            instance,
+                            NULL);    // lpParam
+
+    if(window == NULL)
     {
-        return false;
+      SM_ASSERT(false, "Failed to create Windows Window");
+      return false;
     }
 
-    // WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX
-    int dwStyle = WS_OVERLAPPEDWINDOW;
-
-    PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
-    PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr;
-
-    // Fake Window initializing OpenGL
+    HDC fakeDC = GetDC(window);
+    if(!fakeDC)
     {
-        window = CreateWindowExA(0, title, // This references lpszClassName from wc
-                                    title,      // This is the actual Title
-                                    dwStyle,
-                                    100,
-                                    100,
-                                    width,
-                                    height,
-                                    NULL,       // Parent
-                                    NULL,       // Menu
-                                    instance,
-                                    NULL);      // lpParam
-        if (window == NULL)
-        {
-            SM_ASSERT(false, "Failed to create Windows Window");
-            return false;
-        }
+      SM_ASSERT(false, "Failed to get HDC");
+      return false;
+    }
 
-        HDC fakeDC = GetDC(window);
-        if(!fakeDC)
-        {
-          SM_ASSERT(false, "Failed to get HDC");
-          return false;
-        }
+    PIXELFORMATDESCRIPTOR pfd = {0};
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+    pfd.cAlphaBits = 8;
+    pfd.cDepthBits = 24;
 
-        PIXELFORMATDESCRIPTOR pfd = {0};
-        pfd.nSize = sizeof(pfd);
-        pfd.nVersion = 1;
-        pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-        pfd.iPixelType = PFD_TYPE_RGBA;
-        pfd.cColorBits = 32;
-        pfd.cAlphaBits = 8;
-        pfd.cDepthBits = 24;
-
-        int pixelFormat = ChoosePixelFormat(fakeDC, &pfd);
-        if(!pixelFormat)
-        {
-          SM_ASSERT(false, "Failed to choose pixel Format");
-          return false;
-        }
-
-        if(!SetPixelFormat(fakeDC, pixelFormat, &pfd))
-        {
-          SM_ASSERT(false, "Failed to set pixel format");
-          return false;
-        }
-
-        // Create a Handle to a fake OpenGL Rendering Context
-        HGLRC fakeRC = wglCreateContext(fakeDC);
-        if(!fakeRC)
-        {
-          SM_ASSERT(false, "Failed to create Render context");
-          return false;
-        }
-
-        if(!wglMakeCurrent(fakeDC, fakeRC))
-        {
-          SM_ASSERT(false, "Failed to make current");
-          return false;
-        }
-
-        wglChoosePixelFormatARB = 
-          (PFNWGLCHOOSEPIXELFORMATARBPROC)platform_load_gl_function("wglChoosePixelFormatARB");
-        wglCreateContextAttribsARB =
-          (PFNWGLCREATECONTEXTATTRIBSARBPROC)platform_load_gl_function("wglCreateContextAttribsARB");
-        wglSwapIntervalEXT_ptr =
-          (PFNWGLSWAPINTERVALEXTPROC)platform_load_gl_function("wglSwapIntervalEXT");
-
-        if(!wglCreateContextAttribsARB || !wglChoosePixelFormatARB)
-        {
-          SM_ASSERT(false, "Failed to load OpenGL functions");
-          return false;
-        }
-
-        // Clean up the take stuff
-        wglMakeCurrent(fakeDC, 0);
-        wglDeleteContext(fakeRC);
-        ReleaseDC(window, fakeDC);
-
-        // Can't reuse the same (Device)Context, 
-        // because we already called "SetPixelFormat"
-        DestroyWindow(window);  
-
-    } 
-
-    // Actual OpenGL initialization
+    int pixelFormat = ChoosePixelFormat(fakeDC, &pfd);
+    if(!pixelFormat)
     {
-        // Add in the border size of the window
-        {
-          RECT borderRect = {};
-          AdjustWindowRectEx(&borderRect, dwStyle, 0, 0);
-    
-          width += borderRect.right - borderRect.left;
-          height += borderRect.bottom - borderRect.top;
-        }
-    
-        window = CreateWindowExA(0, title, // This references lpszClassName from wc
-                                title,    // This is the actual Title
-                                dwStyle,
-                                100,
-                                100,
-                                width,
-                                height,
-                                NULL,     // parent
-                                NULL,     // menu
-                                instance,
-                                NULL);    // lpParam
-    
-        if(window == NULL)
-        {
-          SM_ASSERT(false, "Failed to create Windows Window");
-          return false;
-        }
-    
-        dc = GetDC(window);
-        if(!dc)
-        {
-          SM_ASSERT(false, "Failed to get DC");
-          return false;
-        }
-    
-        const int pixelAttribs[] =
-        {
-          WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
-          WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
-          WGL_DOUBLE_BUFFER_ARB,  GL_TRUE,
-          WGL_SWAP_METHOD_ARB,    WGL_SWAP_COPY_ARB,
-          WGL_PIXEL_TYPE_ARB,     WGL_TYPE_RGBA_ARB,
-          WGL_ACCELERATION_ARB,   WGL_FULL_ACCELERATION_ARB,
-          WGL_COLOR_BITS_ARB,     32,
-          WGL_ALPHA_BITS_ARB,     8,
-          WGL_DEPTH_BITS_ARB,     24,
-          0 // Terminate with 0, otherwise OpenGL will throw an Error!
-        };
-    
-        UINT numPixelFormats;
-        int pixelFormat = 0;
-        if(!wglChoosePixelFormatARB(dc, pixelAttribs,
-                                    0, // Float List
-                                    1, // Max Formats
-                                    &pixelFormat,
-                                    &numPixelFormats))
-        {
-          SM_ASSERT(0, "Failed to wglChoosePixelFormatARB");
-          return false;
-        }
-    
-        PIXELFORMATDESCRIPTOR pfd = {0};
-        DescribePixelFormat(dc, pixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
-    
-        if(!SetPixelFormat(dc, pixelFormat, &pfd))
-        {
-          SM_ASSERT(0, "Failed to SetPixelFormat");
-          return true;
-        }
-    
-        const int contextAttribs[] =
-        {
-          WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-          WGL_CONTEXT_MINOR_VERSION_ARB, 3,
-          WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
-          WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,
-          0 // Terminate the Array
-        };
-    
-        HGLRC rc = wglCreateContextAttribsARB(dc, 0, contextAttribs);
-        if(!rc)
-        {
-          SM_ASSERT(0, "Failed to crate Render Context for OpenGL");
-          return false;
-        }
-    
-        if(!wglMakeCurrent(dc, rc))
-        {
-          SM_ASSERT(0, "Faield to wglMakeCurrent");
-          return false;
-        }
-      }
+      SM_ASSERT(false, "Failed to choose pixel Format");
+      return false;
+    }
 
-    ShowWindow(window, SW_SHOW);
+    if(!SetPixelFormat(fakeDC, pixelFormat, &pfd))
+    {
+      SM_ASSERT(false, "Failed to set pixel format");
+      return false;
+    }
 
-    return true;
+    // Create a Handle to a fake OpenGL Rendering Context
+    HGLRC fakeRC = wglCreateContext(fakeDC);
+    if(!fakeRC)
+    {
+      SM_ASSERT(false, "Failed to create Render context");
+      return false;
+    }
+
+    if(!wglMakeCurrent(fakeDC, fakeRC))
+    {
+      SM_ASSERT(false, "Failed to make current");
+      return false;
+    }
+
+    wglChoosePixelFormatARB = 
+      (PFNWGLCHOOSEPIXELFORMATARBPROC)platform_load_gl_function("wglChoosePixelFormatARB");
+    wglCreateContextAttribsARB =
+      (PFNWGLCREATECONTEXTATTRIBSARBPROC)platform_load_gl_function("wglCreateContextAttribsARB");
+
+    if(!wglCreateContextAttribsARB || !wglChoosePixelFormatARB)
+    {
+      SM_ASSERT(false, "Failed to load OpenGL functions");
+      return false;
+    }
+
+    // Clean up the take stuff
+    wglMakeCurrent(fakeDC, 0);
+    wglDeleteContext(fakeRC);
+    ReleaseDC(window, fakeDC);
+
+    // Can't reuse the same (Device)Context, 
+    // because we already called "SetPixelFormat"
+    DestroyWindow(window);  
+  }
+
+  // Actual OpenGL initialization
+  {
+    // Add in the border size of the window
+    {
+      RECT borderRect = {};
+      AdjustWindowRectEx(&borderRect, dwStyle, 0, 0);
+
+      width += borderRect.right - borderRect.left;
+      height += borderRect.bottom - borderRect.top;
+    }
+
+    window = CreateWindowExA(0, title, // This references lpszClassName from wc
+                            title,    // This is the actual Title
+                            dwStyle,
+                            100,
+                            100,
+                            width,
+                            height,
+                            NULL,     // parent
+                            NULL,     // menu
+                            instance,
+                            NULL);    // lpParam
+
+    if(window == NULL)
+    {
+      SM_ASSERT(false, "Failed to create Windows Window");
+      return false;
+    }
+
+    dc = GetDC(window);
+    if(!dc)
+    {
+      SM_ASSERT(false, "Failed to get DC");
+      return false;
+    }
+
+    const int pixelAttribs[] =
+    {
+      WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+      WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+      WGL_DOUBLE_BUFFER_ARB,  GL_TRUE,
+      WGL_SWAP_METHOD_ARB,    WGL_SWAP_COPY_ARB,
+      WGL_PIXEL_TYPE_ARB,     WGL_TYPE_RGBA_ARB,
+      WGL_ACCELERATION_ARB,   WGL_FULL_ACCELERATION_ARB,
+      WGL_COLOR_BITS_ARB,     32,
+      WGL_ALPHA_BITS_ARB,     8,
+      WGL_DEPTH_BITS_ARB,     24,
+      0 // Terminate with 0, otherwise OpenGL will throw an Error!
+    };
+
+    UINT numPixelFormats;
+    int pixelFormat = 0;
+    if(!wglChoosePixelFormatARB(dc, pixelAttribs,
+                                0, // Float List
+                                1, // Max Formats
+                                &pixelFormat,
+                                &numPixelFormats))
+    {
+      SM_ASSERT(0, "Failed to wglChoosePixelFormatARB");
+      return false;
+    }
+
+    PIXELFORMATDESCRIPTOR pfd = {0};
+    DescribePixelFormat(dc, pixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
+
+    if(!SetPixelFormat(dc, pixelFormat, &pfd))
+    {
+      SM_ASSERT(0, "Failed to SetPixelFormat");
+      return true;
+    }
+
+    const int contextAttribs[]
+    {
+      WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+      WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+      WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+      WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,
+      0 // Terminate the Array
+    };
+
+    HGLRC rc = wglCreateContextAttribsARB(dc, 0, contextAttribs);
+    if(!rc)
+    {
+      SM_ASSERT(0, "Failed to crate Render Context for OpenGL");
+      return false;
+    }
+
+    if(!wglMakeCurrent(dc, rc))
+    {
+      SM_ASSERT(0, "Faield to wglMakeCurrent");
+      return false;
+    }
+  }
+
+  ShowWindow(window, SW_SHOW);
+
+  return true;
 }
 
 void platform_update_window()
 {
-  
   // Clear the transitionCount for every key
   {
     for (int keyCode = 0; keyCode < KEY_COUNT; keyCode++)
@@ -340,28 +332,25 @@ void platform_update_window()
 
 void* platform_load_gl_function(char* funName)
 {
-    PROC proc = wglGetProcAddress(funName);
-
-    if (!proc)
+  PROC proc = wglGetProcAddress(funName);
+  if(!proc)
+  {
+    static HMODULE openglDLL = LoadLibraryA("opengl32.dll");
+    proc = GetProcAddress(openglDLL, funName);
+    if(!proc)
     {
-        static HMODULE openglDLL = LoadLibraryA("opengl32.dll");
-        proc = GetProcAddress(openglDLL, funName);
-
-        if (!proc)
-        {
-            SM_ASSERT(false, "Failed to load gl function %s", "glCreateProgram");
-            return nullptr;
-        }
+      SM_ASSERT(false, "Failed to load gl function %s", "glCreateProgram");
+      return nullptr;
     }
-    
-    return (void*)proc;
+  }
+
+  return (void*)proc;
 }
 
 void platform_swap_buffers()
 {
-    SwapBuffers(dc);
+  SwapBuffers(dc);
 }
-
 
 void* platform_load_dynamic_library(char* dll)
 {
