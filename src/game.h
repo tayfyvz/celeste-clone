81f481b8ee2574e1fe5cbb803a864205615b6a03
tayfyvz <tyfn51@gmail.com>
2025-05-15 13:15:22 -0400

    Fixed delta time imported.

--- src/game.h
+++ src/game.h
@@ -7,14 +7,16 @@
 // #############################################################################
 //                           Game Globals
 // #############################################################################
+constexpr int UPDATES_PER_SECOND = 60;
+constexpr double UPDATE_DELAY = 1.0 / UPDATES_PER_SECOND;
 constexpr int WORLD_WIDTH = 320;
 constexpr int WORLD_HEIGHT = 180;
 constexpr int TILESIZE = 8;
 constexpr IVec2 WORLD_GRID = {WORLD_WIDTH / TILESIZE, WORLD_HEIGHT / TILESIZE};
+
 // #############################################################################
 //                           Game Structs
 // #############################################################################
-
 enum GameInputType
 {
   MOVE_LEFT,
@@ -34,35 +36,42 @@ struct KeyMapping
   Array<KeyCodeID, 3> keys;
 };
 
-
 struct Tile
 {
   int neighbourMask;
   bool isVisible;
 }; 
 
+struct Player
+{
+  IVec2 pos;
+  IVec2 prevPos;
+};
+
 struct GameState
 {
+  float updateTimer;
   bool initialized = false;
-  IVec2 playerPos;
+
+  Player player;
   
   Array<IVec2, 21> tileCoords;
   Tile worldGrid[WORLD_GRID.x][WORLD_GRID.y];
   KeyMapping keyMappings[GAME_INPUT_COUNT];
 };
 
-
 // #############################################################################
 //                           Game Globals
 // #############################################################################
 static GameState* gameState;
-
-
 
 // #############################################################################
 //                           Game Functions (Exposed)
 // #############################################################################
 extern "C"
 {
-  EXPORT_FN void update_game(GameState* gameStateIn, RenderData* renderDataIn, Input* inputIn);
+  EXPORT_FN void update_game(GameState* gameStateIn, 
+                             RenderData* renderDataIn, 
+                             Input* inputIn, 
+                             float dt);
 }
